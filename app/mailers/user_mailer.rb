a127259f6e6484b877e9599fcec58f911ecdb9ea
Nilu <nlhpandey@gmail.com>
2012-09-29 14:12:26 -0400

    edited site

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "nilesh@saberr.com"
   def welcome_email(user)
       @user = user
-      @url  = "http://saberr.herokuapp/welcome_email.html.erb"
+      @url  = "http://saberr.nilumailtester/welcome_email.html.erb"
       mail(:to => user.email, :subject => "Welcome to My Awesome Site")
   end
 end
